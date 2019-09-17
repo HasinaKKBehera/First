8fc8b8e426f4fb4a6c9657b61f1194f8eb2a6c7c
HasinaKKBehera <55384255+HasinaKKBehera@users.noreply.github.com>
2019-09-17 16:53:51 +0530

    Add files via upload

---
+++ HashSet.java
@@ -0,0 +1,16 @@
+package com.nt.collction;
+
+public class HashSet {
+
+	public static void main(String[] args) {
+	HashSet sh new = HashSet();
+		
+		sh.add("a");
+		sh.add("b");
+		sh.add("c");
+		sh.add("d");
+		System.out.println(sh);
+
+	}
+
+}
